8199fe26b7eb0730ad98a3cdfafc7d94a82b6990
Bababsg998 <148156989+Bababsg998@users.noreply.github.com>
2023-10-16 18:51:25 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("Modelo iniciado!");
+}
+
+function draw(){
+    image(video, 0, 0, 300, 300); 
+
+    
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+            console.log(results);
+            console.log("Naris x =" + results[0].pose.nose.x);
+            console.log("Naris y =" + results[0].pose.nose.y);
+    }} 
+
+function tirarFoto(){
+
+}
